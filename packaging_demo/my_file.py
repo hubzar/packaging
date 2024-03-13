161d72a1a4050e2fd50fb289802a5acbb4fd3f18
hubzar <doprzodu999@gmail.com>
2024-03-13 18:31:51 +0100

    initial commit

---
+++ packaging_demo/my_file.py
@@ -0,0 +1,13 @@
+"""excercise file"""
+
+import numpy as np
+
+from packaging_demo.my_folder.my_nested_file import CONSTANT as CONSTANT2
+from packaging_demo.my_other_file import CONSTANT
+
+MY_ARRAY = np.array([1, 2, 3])
+STRING_EXAMPLE = 'example string "a" '
+print(MY_ARRAY)
+
+print(CONSTANT)
+print(CONSTANT2)

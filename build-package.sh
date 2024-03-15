c100a01ae2492f246d08b652f071d40e7c396b1a
hubzar <doprzodu999@gmail.com>
2024-03-15 19:54:32 +0100

    getting more familiar with pyproject.toml

---
+++ build-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# clean up cached files
+rm -rf dist build
+find . \
+  -type d \
+  \( \
+  -name "*cache*" \
+  -o -name "*.dist-info" \
+  -o -name "*.egg-info" \
+  \) \
+  -not -path "./venv/*" \
+  -exec rm -r {} +
+
+# rebuild and uznzip the wheel
+python -m build --sdist --wheel ./
+pip install .
+
+# Use those if you want to see a preview of package in dist.
+# cd dist
+# unzip *.whl
+# cd ..

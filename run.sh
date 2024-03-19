75a97906880278a4201daaeb2a49d049c677d8aa
hubzar <doprzodu999@gmail.com>
2024-03-19 18:18:25 +0100

    getting ready for git actions

---
+++ run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+function load-dotenv {
+    while read -r line; do
+        export "$line"
+    done < <(grep -v '^#' "$THIS_DIR/.env" | grep -v '^$')
+}
+
+function install {
+    python -m pip install --editable "$THIS_DIR/[dev]"
+}
+
+function lint {
+    pre-commit run --all-files
+}
+
+function build {
+    python -m build --sdist --wheel "$THIS_DIR/"
+}
+
+function release:test {
+    clean
+    build
+    publish:test
+}
+
+function publish:test {
+    load-dotenv
+    twine upload dist/* \
+        --repository testpypi \
+        --username=__token__ \
+        --password="$TEST_PYPI_TOKEN"
+}
+
+function clean {
+    rm -rf dist build
+    find . \
+        -type d \
+        \( \
+        -name "*cache*" \
+        -o -name "*.dist-info" \
+        -o -name "*.egg-info" \
+        \) \
+        -not -path "./venv/*" \
+        -exec rm -r {} +
+}
+
+function help {
+    echo "$0 <task> <args>"
+    echo "Tasks:"
+    compgen -A function | cat -n
+}
+
+TIMEFORMAT="Task completed in %3lR"
+time ${@:-help}
